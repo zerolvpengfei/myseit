14e7e1ab009cefebfb1818b94a9afb375ab3819f
zero <119064490@qq.com>
2019-05-09 15:43:10 +0800

    day-01 helloapp

--- myseit/urls.py
+++ myseit/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from helloapp import views
 urlpatterns = [
+    path('index/',views.hello),
     path('admin/', admin.site.urls),
+
 ]
