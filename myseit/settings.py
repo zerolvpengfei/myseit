5934091156d9d52ac552305fc4f4333877fd8767
zero <119064490@qq.com>
2019-05-13 10:42:01 +0800

    瞎练习

--- myseit/settings.py
+++ myseit/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'myseit.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'helloapp2/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
